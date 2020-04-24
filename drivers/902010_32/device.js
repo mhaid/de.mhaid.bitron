484fb574b0e13bde55d54f56fb23ba19b40a48e8
mhaid <mhaid2016@outlook.com>
2020-04-25 00:45:09 +0200

    Heat only Feature

--- drivers/902010_32/device.js
+++ drivers/902010_32/device.js
@@ -3,6 +3,10 @@
 const Homey = require('homey');
 const ZigBeeDevice = require('homey-meshdriver').ZigBeeDevice;
 
+var localTempVar = 2100;
+var occupiedHeatingSetpointVar = 2100;
+var occupiedHeatingSetpointVarTmp = 2100;
+
 class Bitron_902010_32 extends ZigBeeDevice {
 
 	// this method is called when the device is inited and values are changed
@@ -19,14 +23,72 @@ class Bitron_902010_32 extends ZigBeeDevice {
 		this.registerCapability('target_temperature', 'hvacThermostat', {
 			set: 'occupiedHeatingSetpoint',
 			setParser(value) {
-				this.node.endpoints[0].clusters.hvacThermostat.write('occupiedHeatingSetpoint',
-					Math.round(value * 1000 / 10))
-					.then(res => {
-						this.log('write occupiedHeatingSetpoint: ', res);
-					})
-					.catch(err => {
-						this.error('Error write occupiedHeatingSetpoint: ', err);
-					});
+
+				var settings = this.getSettings();
+				if(settings.heatOnly_enabled == false) {
+					if(localTempVar < value) {
+						//set mode heat
+						this.node.endpoints[0].clusters.hvacThermostat.write('systemMode',
+							4)
+							.then(res => {
+								this.log('write systemMode: ', res);
+
+								//set occupiedHeatingSetpoint
+								this.node.endpoints[0].clusters.hvacThermostat.write('occupiedHeatingSetpoint',
+									Math.round(value * 1000 / 10))
+									.then(res => {
+										this.log('write occupiedHeatingSetpoint: ', res);
+									})
+									.catch(err => {
+										this.error('Error write occupiedHeatingSetpoint: ', err);
+									});
+							})
+							.catch(err => {
+								this.error('Error write systemMode: ', err);
+							});
+					} else if(localTempVar > value) {
+						//set mode cool
+						this.node.endpoints[0].clusters.hvacThermostat.write('systemMode',
+							3)
+							.then(res => {
+								this.log('write systemMode: ', res);
+
+								//set occupiedCoolingSetpoint
+								this.node.endpoints[0].clusters.hvacThermostat.write('occupiedCoolingSetpoint',
+									Math.round(value * 1000 / 10))
+									.then(res => {
+										this.log('write occupiedCoolingSetpoint: ', res);
+									})
+									.catch(err => {
+										this.error('Error write occupiedCoolingSetpoint: ', err);
+									});
+							})
+							.catch(err => {
+								this.error('Error write systemMode: ', err);
+							});
+					} else {
+						//set mode off
+						this.node.endpoints[0].clusters.hvacThermostat.write('systemMode',
+							0)
+							.then(res => {
+								this.log('write systemMode: ', res);
+							})
+							.catch(err => {
+								this.error('Error write systemMode: ', err);
+							});
+					}
+				} else {
+					occupiedHeatingSetpointVarTmp = value;
+					//set occupiedHeatingSetpoint
+					this.node.endpoints[0].clusters.hvacThermostat.write('occupiedHeatingSetpoint',
+						Math.round(value * 1000 / 10))
+						.then(res => {
+							this.log('write occupiedHeatingSetpoint: ', res);
+						})
+						.catch(err => {
+							this.error('Error write occupiedHeatingSetpoint: ', err);
+						});
+				}
 				return null;
 			},
 			get: 'occupiedHeatingSetpoint',
@@ -52,22 +114,39 @@ class Bitron_902010_32 extends ZigBeeDevice {
 			},
 		});
 		// thermostat mode
-		if (this.hasCapability('thermostat_mode')) {
-			this.registerCapability('thermostat_mode', 'hvacThermostat', {
-				get: 'systemMode',
-				reportParser(value) {
-					if(value == 0) { return "off"; } //OFF
-					else if(value == 3) { return "cool"; } //Cooling
-					else if(value == 4) { return "heat"; } //Heating
-					else { return "heat"; } //Default (Heating)
-				},
-				report: 'systemMode',
-				getOpts: {
-					getOnStart: true,
-					getOnOnline: true,
-				},
-			});
-		}
+// 		if (this.hasCapability('thermostat_mode')) {
+//			this.registerCapability('thermostat_mode', 'hvacThermostat', {
+//				set: 'systemMode',
+// 				setParser(value) {
+// 					var sendValue = 4;
+// 					if(value == "off") { sendValue = 0; } //OFF
+// 					else if(value == "cool") { sendValue = 3; } //Cooling
+// 					else if(value == "heat") { sendValue = 4; } //Heating
+
+// 					this.node.endpoints[0].clusters.hvacThermostat.write('systemMode',
+// 						sendValue)
+// 						.then(res => {
+// 							this.log('write systemMode: ', res);
+// 						})
+// 						.catch(err => {
+// 							this.error('Error write systemMode: ', err);
+// 						});
+// 					return null;
+// 				},
+// 				get: 'systemMode',
+// 				reportParser(value) {
+// 					if(value == 0) { return "off"; } //OFF
+// 					else if(value == 3) { return "cool"; } //Cooling
+// 					else if(value == 4) { return "heat"; } //Heating
+// 					else { return "heat"; } //Default (Heating)
+// 				},
+// 				report: 'systemMode',
+// 				getOpts: {
+// 					getOnStart: true,
+// 					getOnOnline: true,
+// 				},
+// 			});
+// 		}
 		// battery
 //		if (this.hasCapability('measure_battery')) {
 //			this.registerCapability('measure_battery', 'genPowerCfg', {
@@ -105,24 +184,15 @@ class Bitron_902010_32 extends ZigBeeDevice {
 		this.registerAttrReportListener('hvacThermostat', 'occupiedHeatingSetpoint', 1, 300, 10, value => {
 			const parsedValue = Math.round((value / 100) * 10) / 10;
 			this.log('hvacThermostat - occupiedHeatingSetpoint: ', value, parsedValue);
+			occupiedHeatingSetpointVar = parsedValue;
 			this.setCapabilityValue('target_temperature', parsedValue);
 		}, 0);
 		// local temperature
 		this.registerAttrReportListener('hvacThermostat', 'localTemp', 1, 300, 10, value => {
 			const parsedValue = Math.round((value / 100) * 10) / 10;
 			this.log('hvacThermostat - localTemp: ', value, parsedValue);
+			localTempVar = parsedValue;
 			this.setCapabilityValue('measure_temperature', parsedValue);
-		}, 0);
-		// thermostat mode
-		this.registerAttrReportListener('hvacThermostat', 'systemMode', 1, 300, 4, value => {
-			let parsedValue = "heat";
-			
-			if(value == 0) { parsedValue = "off"; } //OFF
-			else if(value == 3) { parsedValue = "cool"; } //Cooling
-			else if(value == 4) { parsedValue = "heat"; } //Heating (default)
-			
-			this.log('hvacThermostat - systemMode: ', parsedValue);
-			this.setCapabilityValue('thermostat_mode', parsedValue);
 		}, 0);
 		// maesure battery
 //		this.registerAttrReportListener('genPowerCfg', 'batteryPercentageRemaining', 1, 3600, null, value => {
@@ -139,7 +209,6 @@ class Bitron_902010_32 extends ZigBeeDevice {
 				this.setCapabilityValue('measure_battery', Math.round((value - 23) / (30 - 23) * 100));
 			}
 		}, 0);
-
 	}
 
 	// local settings changed
@@ -162,8 +231,45 @@ class Bitron_902010_32 extends ZigBeeDevice {
 					this.log(err);
 				});
 		}
-	}
 
+		//ctrlSeqeOfOper changed
+		if (changedKeysArr.includes('heatOnly_enabled')) {
+			this.log('heatOnly_enabled: ', newSettingsObj.heatOnly_enabled);
+
+			var value = 4;
+			if(newSettingsObj.heatOnly_enabled == true){
+				value = 2;
+			}
+
+			this.node.endpoints[0].clusters.hvacThermostat.write('ctrlSeqeOfOper', value)
+				.then(result => {
+					this.log('ctrlSeqeOfOper: ', result);
+					if(newSettingsObj.heatOnly_enabled){
+						this.node.endpoints[0].clusters.hvacThermostat.write('systemMode', 4)
+							.then(result => {
+								this.log('systemMode: ', result);
+								callback(null, true);
+							})
+							.catch(err => {
+								this.log('could not write systemMode', err);
+								callback(err, false);
+							});
+					} else {
+						callback(null, true);
+					}
+				})
+				.catch(err => {
+					this.log('could not write ctrlSeqeOfOper', err);
+					callback(err, false);
+				});
+		}
+
+		//ctrlSeqeOfOper changed
+//		if (changedKeysArr.includes('intelMode_enabled')) {
+//			this.log('intelMode_enabled: ', newSettingsObj.intelMode_enabled);
+//			callback(null, true);
+//		}
+	}
 }
 
 module.exports = Bitron_902010_32;
