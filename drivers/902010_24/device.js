480b4a225b287e790d04244f93b4d8d8139e1f61
mhaid <mhaid2016@outlook.com>
2020-11-05 14:20:59 +0100

    Add support for 902010/24

---
+++ drivers/902010_24/device.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Homey = require('homey');
+const { ZigBeeDevice } = require('homey-zigbeedriver');
+const { CLUSTER } = require('zigbee-clusters');
+
+class Bitron_902010_24 extends ZigBeeDevice 
+{
+
+	// this method is called when the device is inited and values are changed
+	async onNodeInit({ zclNode }) {
+
+		// enable debugging
+		//this.enableDebug();
+
+		// print the node's info to the console
+		this.printNode();
+
+		// capabilities
+		// alarm_smoke
+		//this.registerCapability('alarm_smoke', CLUSTER.IAS_ZONE);
+
+		// Capture the zoneStatusChangeNotification
+		zclNode.endpoints[1].clusters.iasZone
+			.onZoneStatusChangeNotification = payload => {
+			this.onIASZoneStatusChangeNoficiation(payload);
+		};
+    }
+
+    onIASZoneStatusChangeNoficiation({
+		zoneStatus, extendedStatus, zoneId, delay,
+	}) {
+		this.log('IASZoneStatusChangeNotification:', zoneStatus, extendedStatus, zoneId, delay);
+		this.setCapabilityValue('alarm_smoke', zoneStatus.alarm1);
+		this.setCapabilityValue('alarm_battery', zoneStatus.battery);
+	}
+}
+
+module.exports = Bitron_902010_24;
