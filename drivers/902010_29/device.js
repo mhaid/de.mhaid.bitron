3311212fce7dfa73a84fb3f51afb20d5f4d5167b
Morris Haid <44113328+mhaid@users.noreply.github.com>
2021-09-18 00:40:24 +0200

    Support for /24 and /29

--- drivers/902010_29/device.js
+++ drivers/902010_29/device.js
@@ -6,7 +6,7 @@ const { ZigBeeDevice } = require('homey-zigbeedriver');
 const { Cluster,CLUSTER } = require('zigbee-clusters');
 Cluster.addCluster(BitronSpecificIASWDCluster);
 
-class Bitron_902010_24 extends ZigBeeDevice 
+class Bitron_902010_29 extends ZigBeeDevice 
 {
 
 	// this method is called when the device is inited and values are changed
@@ -19,8 +19,11 @@ class Bitron_902010_24 extends ZigBeeDevice
 		this.printNode();
 
 		// capabilities
-		if (!this.hasCapability('alarm_smoke')) {
-			this.addCapability('alarm_smoke');
+		if (!this.hasCapability('alarm_generic')) {
+			this.addCapability('alarm_generic');
+		}
+		if (!this.hasCapability('alarm_tamper')) {
+			this.addCapability('alarm_tamper');
 		}
 		if (!this.hasCapability('alarm_battery')) {
 			this.addCapability('alarm_battery');
@@ -33,35 +36,36 @@ class Bitron_902010_24 extends ZigBeeDevice
 		};
 
 		// Flow Actions
-		let sirenActionTimer = this.homey.flow.getActionCard('24_siren_timer');
+		let sirenActionTimer = this.homey.flow.getActionCard('29_siren_timer');
 		sirenActionTimer.registerRunListener(async (args, state) => {
 			try {
 				//await zclNode.endpoints[1].clusters.iasWD.startWarning({warningMode: 1, strobe: 'off',sirenLevel: 'medium'});
 				await zclNode.endpoints[1].clusters.iasWD.startWarning({warningMode: 'on,off,medium', warningDuration: args.duration, strobeDutyCycle: 50, storbeLevel: 'medium'});
-				this.setCapabilityValue('alarm_smoke', true);
+				this.setCapabilityValue('alarm_generic', true);
 			} catch (error) {
 				console.log(error);
 			}
 			return true;
 		});
 
-		let sirenActionOn = this.homey.flow.getActionCard('24_siren_on');
+		let sirenActionOn = this.homey.flow.getActionCard('29_siren_on');
 		sirenActionOn.registerRunListener(async (args, state) => {
 			try {
 				//await zclNode.endpoints[1].clusters.iasWD.startWarning({warningMode: 1, strobe: 'off',sirenLevel: 'medium'});
 				await zclNode.endpoints[1].clusters.iasWD.startWarning({warningMode: 'on,off,medium', warningDuration: 0, strobeDutyCycle: 50, storbeLevel: 'medium'});
-				this.setCapabilityValue('alarm_smoke', true);
+				this.setCapabilityValue('alarm_generic', true);
 			} catch (error) {
 				console.log(error);
 			}
 			return true;
 		});
-		let sirenActionOff = this.homey.flow.getActionCard('24_siren_off');
+
+		let sirenActionOff = this.homey.flow.getActionCard('29_siren_off');
 		sirenActionOff.registerRunListener(async (args, state) => {
 			try {
 				//await zclNode.endpoints[1].clusters.iasWD.startWarning({warningMode: 0, strobe: 'off',sirenLevel: 'low'});
 				await zclNode.endpoints[1].clusters.iasWD.startWarning({warningMode: 'off,off,low', warningDuration: 0, strobeDutyCycle: 0, storbeLevel: 'low'});
-				this.setCapabilityValue('alarm_smoke', false);
+				this.setCapabilityValue('alarm_generic', false);
 			} catch (error) {
 				console.log(error);
 			}
@@ -71,11 +75,12 @@ class Bitron_902010_24 extends ZigBeeDevice
 
     onIASZoneStatusChangeNoficiation({zoneStatus, extendedStatus, zoneId, delay}) {
 		this.log('IASZoneStatusChangeNotification:', zoneStatus, extendedStatus, zoneId, delay);
-		this.log('IASZoneStatusChangeNotification (Parsed):',zoneStatus.alarm1,zoneStatus.battery);
-
-		this.setCapabilityValue('alarm_smoke', zoneStatus.alarm1);
+		this.log('IASZoneStatusChangeNotification (Parsed):',zoneStatus.alarm1,zoneStatus.tamper,zoneStatus.battery);
+		
+		this.setCapabilityValue('alarm_generic', zoneStatus.alarm1);
+		this.setCapabilityValue('alarm_tamper', zoneStatus.tamper);
 		this.setCapabilityValue('alarm_battery', zoneStatus.battery);
 	}
 }
 
-module.exports = Bitron_902010_24;
+module.exports = Bitron_902010_29;
